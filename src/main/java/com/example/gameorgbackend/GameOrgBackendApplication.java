0d30b6b7d82cc8278aa919a890199577a73f31de
mz403079 <mz403079@gmail.com>
2021-10-15 19:58:17 +0200

    init commit

---
+++ src/main/java/com/example/gameorgbackend/GameOrgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameorgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOrgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOrgBackendApplication.class, args);
+	}
+
+}
