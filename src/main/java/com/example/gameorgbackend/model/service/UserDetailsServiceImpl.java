abc8e839f1dd2f30bda88cd26248db2e6879f509
mz403079 <mz403079@gmail.com>
2021-11-02 15:18:49 +0100

    added setup for JWT

---
+++ src/main/java/com/example/gameorgbackend/model/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.gameorgbackend.model.service;
+
+import com.example.gameorgbackend.model.entity.User;
+import com.example.gameorgbackend.model.repository.UserRepository;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(
+            () -> new UsernameNotFoundException("User Not Found with username: " + username));
+    return UserDetailsImpl.build(user);
+  }
+
+}
